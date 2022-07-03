1bf93afa1ea2d8d8c4438fee4824fd5e2d137498
Melany <melanyac260@gmail.com>
2022-07-03 05:16:39 -0500

    Cambios-01

--- src/pages/Contenido.js
+++ src/pages/Contenido.js
@@ -1,19 +1,38 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import Typewriter from 'typewriter-effect';
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import '../estilosC.css';
+
+import ReactPlayer from 'react-player';
+
 const Contenido = () => {
   const settings = {
     dots: true,
     infinite: true,
-    slidesToShow: 3,
+    slidesToShow: 4,
     slidesToScroll: 1,
     autoplay: true,
     speed: 1000,
     autoplaySpeed: 2000,
     cssEase: "linear"
   };
+
+  const videoEl = useRef(null);
+
+const attemptPlay = () => {
+  videoEl &&
+    videoEl.current &&
+    videoEl.current.play().catch(error => {
+      console.error("Error attempting to play", error);
+    });
+};
+
+useEffect(() => {
+  attemptPlay();
+}, []);
+
     return (
       <div>
       <section className="hero_sec">
@@ -41,7 +60,6 @@ const Contenido = () => {
 />
 </p>
           <h1>Descubre tu top ideal</h1>
-          <a href="#" className="btn-primary">Más detalles </a>
         </div>
       </section>
       <section className="sec1">
@@ -69,11 +87,13 @@ const Contenido = () => {
                     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                   </div>
-                  <a href="#" className="btn-primary"> oe mongol presiona acá</a>
                 </div>
               </div>
               <div className="sec_img">
-                <img src="4.PNG" alt=""/>
+                <ReactPlayer url='video1.mp4'
+                loop
+                muted
+                />
               </div>
           </section>
 
@@ -81,37 +101,46 @@ const Contenido = () => {
             <h3>Precios / de rebaja / 1 sol / 2 soles / 3 soles / compra p</h3>
             <div className="sec4_img_group">
               <div className="sec4_img1">
-                <img src="6.PNG" width="400" height="500"/>
+                <img src="9.PNG" width="400" height="500"/>
               </div>
               <div className="sec4_img2">
-                <img src="2.PNG" width="400" height="500"/>
+                <img src="7.PNG" width="400" height="450"/>
               </div>
               <div className="sec4_img3">
-                <img src="3.PNG" width="400" height="500"/>
+                <img src="8.PNG" width="400" height="500"/>
               </div>
             </div>
           </section>
 
           <section>
-          <h1>Descubre tu top ideal</h1>
+          <h6>Nuestros clientes</h6>
         <Slider {...settings}>
           <div>
-            <img src="1.PNG" width="400" height="500"/>
+            <img src="P1.PNG" width="300" height="350"/>
           </div>
           <div>
-            <img src="2.PNG" width="400" height="500"/>
+            <img src="P2.PNG" width="300" height="350"/>
           </div>
           <div>
-            <img src="3.PNG" width="400" height="500"/>
+            <img src="P3.PNG" width="300" height="350"/>
           </div>
           <div>
-            <img src="4.PNG" width="400" height="500"/>
+            <img src="P4.PNG" width="300" height="350"/>
           </div>
           <div>
-            <img src="5.PNG" width="400" height="500"/>
+            <img src="P5.PNG" width="300" height="350"/>
           </div>
           <div>
-            <img src="6.PNG" width="400" height="500"/>
+            <img src="P6.PNG" width="300" height="350"/>
+          </div>
+          <div>
+            <img src="P3.PNG" width="300" height="350"/>
+          </div>
+          <div>
+            <img src="P4.PNG" width="300" height="350"/>
+          </div>
+          <div>
+            <img src="P5.PNG" width="300" height="350"/>
           </div>
         </Slider>
       </section>
--- src/pages/Footer.js
+++ src/pages/Footer.js
@@ -1,73 +1,77 @@
 import React from 'react';
-
+import '../estilosF.css';
+import { FaPaperPlane } from "react-icons/fa";
+import { BsTelephone } from "react-icons/bs";
+import { HiOutlineMail } from "react-icons/hi";
+import {MdGpsFixed} from 'react-icons/md';
+import { FiFacebook } from "react-icons/fi";
+import { FiTwitter } from "react-icons/fi";
+import { FiInstagram } from "react-icons/fi";
 const Footer = () => {
     return (
-            <footer class="page-footer bg-dark">
-                {/* Barra Primera */}
-                <div class="bg-success">
-                    <div class="container">
-                        <div class="row py-4 d-flex align-items-center">
-                            <div class="col-md-12 text-center">
-                                <a href="https://www.facebook.com/" target="_blank"><i class="bi bi-facebook text-white p-2"></i></a>
-                                <a href="https://twitter.com/" target="_blank"><i class="bi bi-twitter text-white p-2"></i></a>
-                                <a href="https://www.instagram.com/" target="_blank"><i class="bi bi-instagram text-white p-2"></i></a>
-                                <a href='https://wa.me/+51979377644' target="_blank"><i class="bi bi-whatsapp text-white p-2"></i></a>
-                            </div>
-                        </div>
-                    </div>
+      <div className="main">
+    		<div className="content">
 
-                </div>
-                <div class="container text-center text-md-left mt-5 text-white">
-                    <div class="row">
-                    {/* Columna numero 1 */}
-                        <div class="col-md-3 mx-auto mb-4">
-                            <h6 class="text-uppercase font-weight-bold">Descripcion</h6>
-                            <hr/>
-                            <p class="m-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
-                        </div>
+             <div className="content_left">
+             	<div className="more_detail">
+             		<h4>ContigoPE</h4>
+             		<p>Ingrese su correo, para recibir notificaciones</p>
 
-                        <div class="col-md-2 mx-auto mb-4">
-                            <h6 class="text-uppercase font-weight-bold">Products</h6>
-                            <hr/> 
-                            <ul class="list-unstyled">
-                                <li class="m-2"><a href="/#">Hombres</a></li>
-                                <li class="m-2"><a href="/#">Mujeres</a></li>
-                                <li class="m-2"><a href="/#">Niños</a></li>
-                                <li class="m-2"> <a href="/#">Mundial QATAR 2022</a></li>
-                            </ul>
-                        </div>
-                        {/* Columna numero 2 */}
-                        {/* <div class="col-md-2 mx-auto mb-4">
-                            <h6 class="text-uppercase font-weight-bold">EN PRUEBA</h6>
-                            <hr/>
-                            <ul class="list-unstyled">
-                                <li class="m-2"><a href="/#">Home</a></li>
-                                <li class="m-2"><a href="/#">About</a></li>
-                                <li class="m-2"><a href="/#">Services</a></li>
-                                <li class="m-2"> <a href="/#">Contact</a></li>
-                            </ul>
-                        </div> */}
-                        {/* Columna numero 3 */}
-                        <div class="col-md-3 mx-auto mb-4">
-                            <h6 class="text-uppercase font-weight-bold">Contactanos</h6>
-                            <hr/>
-                            <ul class="list-unstyled">
-                                <li class="m-2"><i class="fas fa-home mr-3"></i>Av. Camino Real 208, San Isidro 15073</li>
-                                <li class="m-2"><i class="fas fa-envelope mr-3"></i>ContigoPE@gmail.com</li>
-                                <li class="m-2"><i class="fas fa-phone mr-3"></i>+51 979 377 644</li>
-                                <li class="m-2"><i class="fas fa-print mr-3"></i>+51 973 7324 74</li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-                <div class="footer-copyright text-center py-3 text-white">
-                    <p>&copy; Copyright<a href="/#"> ByTeamPython</a></p>
-                    <p>EL CHAMO</p>
+             		<div className="form">
+             			<input type="email" placeholder="Enter Email"/>
+             			<button><FaPaperPlane/></button>
+
+             		</div>
+
+             	</div>
+
+             	<div className="more_detail_2">
+             		<h4>Producto</h4>
+             		<p><a>Polos</a></p>
+             		<p><a>Short</a></p>
+             		<p><a>Buzo</a></p>
+             	</div>
+             </div>
+
+             <div className="content_right">
+             	<div className="more_detail">
+             		<h4>Paises</h4>
+             		<p><a>Perú</a></p>
+             		<p><a>Brasil</a></p>
+             		<p><a>España</a></p>
+             	</div>
+
+             	<div className="more_detail_2">
+             		<h4>Contáctanos</h4>
+             		<p><BsTelephone/> +(01) 5568413</p>
+             		<p><HiOutlineMail/> soporte@contigope.com.pe</p>
+             		<p><MdGpsFixed/> Av. Camino Real 208, San Isidro 15073</p>
+             	</div>
+             </div>
+
+    		</div>
+    		<p id="line"></p>
+
+    		<div className="bottom_content">
+
+                <div className="terms_services">
+                	<p><a>Team Python</a></p>
+                	<p><a>Policy Privacy</a></p>
                 </div>
 
+                <div className="social_media">
+                	<li><FiFacebook/></li>
+                	<li><FiTwitter/></li>
+                	<li><FiInstagram/></li>
 
-            </footer>
-        
+                </div>
+
+                <p id="copyright">@2022 TeamPython.com. All Rights Reserved.</p>
+
+    		</div>
+
+    	</div>
+
     );
 }
 
